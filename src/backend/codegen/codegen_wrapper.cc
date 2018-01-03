de2966575218b3e10d2414f37c2b9bcc0ed24b8a
Jesse Zhang <sbjesse@gmail.com>
2018-01-03 14:29:39 -0800

    Revert "Support avg aggregate function in codegen"
    This reverts commit f38c90640ec9926bf9a9901367dd7adab10acefc.
    
    Signed-off-by: Melanie Plageman <mplageman@pivotal.io>

--- src/backend/codegen/codegen_wrapper.cc
+++ src/backend/codegen/codegen_wrapper.cc
@@ -27,7 +27,6 @@
 
 extern "C" {
 #include "lib/stringinfo.h"
-#include "postgres.h"  // NOLINT(build/include)
 }
 
 using gpcodegen::CodegenManager;
@@ -111,15 +110,6 @@ slot_getattr_regular(TupleTableSlot *slot, int attnum, bool *isnull) {
 int
 att_align_nominal_regular(int cur_offset, char attalign) {
   return att_align_nominal(cur_offset, attalign);
-}
-
-void SET_VARSIZE_regular(void* ptr, size_t len) {
-  SET_VARSIZE(ptr, len);
-}
-
-uint32
-VARSIZE_regular(void* ptr) {
-  return VARSIZE(ptr);
 }
 
 void* ExecVariableListCodegenEnroll(
