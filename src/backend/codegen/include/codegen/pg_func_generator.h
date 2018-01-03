330241dfb67f8b62def651592f0dedade91a7559
Jesse Zhang <sbjesse@gmail.com>
2018-01-03 14:30:07 -0800

    Revert "Set llvm_isNull_ptr to false when the result of codegened expression evaluation is not null"
    This reverts commit bc6faa08b2f9ab677536a3ba0b045106b983024f.
    
    Signed-off-by: Melanie Plageman <mplageman@pivotal.io>

--- src/backend/codegen/include/codegen/pg_func_generator.h
+++ src/backend/codegen/include/codegen/pg_func_generator.h
@@ -285,11 +285,6 @@ class PGIRBuilderFuncGenerator
     llvm::BasicBlock* func_generation_last_block = irb->GetInsertBlock();
     assert(llvm_func_tmp_value->getType() ==
         codegen_utils->GetType<ReturnType>());
-    // Explicitly set isnull to false so that we can cover the case that isnull
-    // has not been initialized to false. This is useful because in codegen
-    // we do not use temporary struct fcinfo.
-    irb->CreateStore(codegen_utils->GetConstant<bool>(false),
-                     llvm_isnull_ptr);
     irb->CreateBr(set_llvm_out_value_block);
 
     // null_argument_block
@@ -507,11 +502,6 @@ class PGGenericFuncGenerator : public  PGFuncGeneratorInterface {
     llvm::BasicBlock* func_generation_last_block = irb->GetInsertBlock();
     assert(llvm_func_generation_tmp_value->getType() ==
            codegen_utils->GetType<ReturnType>());
-    // Explicitly set isnull to false so that we can cover the case that isnull
-    // has not been initialized to false. This is useful because in codegen
-    // we do not use temporary struct fcinfo.
-    irb->CreateStore(codegen_utils->GetConstant<bool>(false),
-                     llvm_isnull_ptr);
     irb->CreateBr(set_llvm_out_value_block);
 
     // set_llvm_out_value_block
