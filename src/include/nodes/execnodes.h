2b08ec0391106adabe59b6e4489a5b39959244a8
Gang Xiong <gxiong@pivotal.io>
2019-03-06 15:17:07 +0800

    temp

--- src/include/nodes/execnodes.h
+++ src/include/nodes/execnodes.h
@@ -2883,6 +2883,7 @@ typedef struct MotionState
 
 	int			numInputSegs;	/* the number of segments on the sending slice */
 	bool		isExplictGatherMotion;
+	int 		numEndOfParamRecved;
 } MotionState;
 
 /*zx
